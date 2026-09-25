a3427828dabb5cb2315d67518447239816155a7a
TMTXCODE <nihadcoder@gmail.com>
2026-09-25 22:02:01 +0330

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,7 +1,29 @@
 name = "TMTXCODE"
-language = "Python"
-focus = "Programming and Networking"
+age = 19
+main_language = "Python"
+field = "Programming and Networking"
 
-print("Name:", name)
-print("Main language:", language)
-print("Focus:", focus)
+skills = [
+    "Python",
+    "C++",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "SQL",
+    "Linux",
+    "Git",
+    "Networking",
+    "API",
+    "JSON"
+]
+
+print("TMTXCODE")
+print()
+print("Age:", age)
+print("Main language:", main_language)
+print("Field:", field)
+print()
+print("Things I'm learning:")
+
+for skill in skills:
+    print("-", skill)
